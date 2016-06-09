29c9b9fe21b0bdabb717a0f57b1271fa9f8a0279
Daniel Slavkovský <setrin@gmail.com>
2016-06-09 13:51:39 +0200

    enviroment_setup - added base configurations

---
+++ build.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	build_dir : 'build',
+	//compile_dir : 'bin',
+	app_files : {
+		js : ['src/**/*.js']
+		//scss : 'src/css/**/*.scss',
+		//jsunit: [ 'src/**/*.spec.js' ]
+	}
+};
