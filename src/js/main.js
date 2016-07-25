13adc563b8cec0d41e91e96500212ac46b985d06
Daniel Slavkovský <setrin@gmail.com>
2016-07-25 22:35:54 +0200

    core index - added postal.js lib, small css change and removed unused lines from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 'use strict';
-var $ = require("jquery");
-import Settings from './core/Settings.js';
+//var $ = require("jquery");
 import World from './core/World.js';
 
 $(function(){
-	var settings = new Settings();
-	var world = new World(settings);
+	var world = new World();
 });
