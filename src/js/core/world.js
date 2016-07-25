6007cc0831beda19e491b6f671ed3549ace7c5a0
Daniel Slavkovský <setrin@gmail.com>
2016-07-25 22:29:25 +0200

    core world - added gameloop, game views, subscribtions

--- src/js/core/world.js
+++ src/js/core/world.js
@@ -1,14 +1,81 @@
 'use strict';
 
+import AssetManager from './Asset_manager.js';
+import Settings from './Settings.js';
+import LoadingView from '../views/Loading_view.js';
+import MenuView from '../views/Menu_view.js';
+
+/**
+*	Name: 'World' class
+*	Desc: Handle all Game states and manage everything
+**/
 export default class {
-	constructor (settings) {
-		this._settings = settings;
-		/*this.GameLoop(function(fps){
-		    console.log(fps);
-		}, this._settings.max_fps);*/
+	constructor () {
+		this._assetManager = new AssetManager();
+		this._settings = new Settings();
+		this._canvas = this._settings.canvas;
+		this._channel = postal.channel("world");
+		this._gameloop_run = true;
+		this._game_state = 0; //0 - Loading, 1 - Menu, 2 - Playing, 3 - Game over
+		//this._entities_index = 0;
+		//this._entities = {}; // {this._entities_index: new Entity(this._entities_index), ...}
+
+		this.subscribe();
+
+		this._loadingView = new LoadingView(this._settings);
+		this._menuView = new MenuView(this._settings);
+
+		this.startGame();
+
+		this._assetManager.startDownload();
 	}
 
-	GameLoop (fn, fps){
+	/*-------------------------------------------------------------
+	*	Function that handles all subscribes and callbacks
+	*-------------------------------------------------------------*/
+	subscribe () {
+		var that = this;
+
+	    this._channel.subscribe("state.change", 
+			function(data) {
+	           	that._game_state = data.state;
+	        }
+	    );
+
+	    this._channel.subscribe("gameloop.stop", 
+			function() {
+	           	that._gameloop_run = false;
+	        }
+	    );
+	}
+
+	/*-------------------------------------------------------------
+	*	Function to start GameLoop - Main loop
+	*-------------------------------------------------------------*/
+	startGame () {
+		var that = this;
+		this.GameLoop(function(fps){
+			switch (that._game_state) {
+				case 0: // Loading
+			    	that._loadingView.render(fps);
+			    	break;
+			    case 1: // Menu
+			    	that._menuView.render(fps);
+			    	break;
+			    case 2: //  Game Play
+			    	
+			    	break;
+			    case 3: //  Game Over
+			    	
+			    	break;
+			}
+		}, this._settings.max_fps);
+	}
+
+	/*-------------------------------------------------------------
+	*	Main GameLoop function to control FPS
+	*-------------------------------------------------------------*/
+	GameLoop (fn, fps) {
 	    var max_fps = fps || 60,
 	    	now,
 	    	delta,
@@ -16,10 +83,13 @@ export default class {
 	    	then = performance.now(),
 
 	    	frames,
-	    	oldtime = 0;
+	    	oldtime = 0,
+	    	that = this;
 
 	    return (function loop(time){
-	        requestAnimationFrame(loop);
+	        if (that._gameloop_run) {
+	        	requestAnimationFrame(loop);
+	        }
 
 	        interval = 1000 / (max_fps);
 	        now = performance.now();
@@ -39,4 +109,6 @@ export default class {
 	        }
 	    }(0));
 	}
+
+	get ctx () {return this._ctx;}
 }
